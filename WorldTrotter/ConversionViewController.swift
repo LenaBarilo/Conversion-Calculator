5852650f17c85d49f8983823a62725d41abbe5fa
yelenabarilo <ybarilo@beachbody.com>
2016-09-30 20:10:27 -0700

    Initializing Converter App repo

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by YelenaBarilo on 9/28/16.
+//  Copyright © 2016 YelenaBarilo. All rights reserved.
+//
+
+import Foundation
